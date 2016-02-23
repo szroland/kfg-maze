0ee46c862c2ea64db4215e3a54de18038d0fd353
szroland <szroland@freemail.hu>
2016-02-24 00:26:59 +0100

    Feladatok

--- Assets/Scripts/BlockMazeDisplay.cs
+++ Assets/Scripts/BlockMazeDisplay.cs
@@ -17,30 +17,7 @@ public class BlockMazeDisplay : MonoBehaviour {
         generator = GetComponent<BlockMazeGenerator>();
         maze = generator.generate();
 
-        for (int i=0; i<maze.MeretX; i++)
-        {
-            for (int j=0; j<maze.MeretY; j++)
-            {
-                LabirintusCella(i, j);
-            }
-        }
-
 	}
-
-    private void LabirintusCella(int i, int j)
-    {
-        Vector3 pos = new Vector3(i * MeretX, 0, j * MeretZ);
-        if (maze.Fal(i, j))
-        {
-            if (Fal != null)
-                Letrehoz(pos, Fal);
-        }
-        else
-        {
-            if (Padlo != null)
-                Letrehoz(pos, Padlo);
-        }
-    }
 
     //Létrehozza a megadott prefab másolatát a pos által definiált relatív pozícióban
     void Letrehoz(Vector3 pos, GameObject epitoElem)
---
+++ README.md
@@ -0,0 +1,25 @@
+# Labirintusok
+
+## 2016.02.24 
+
+### Feladatok
+
+- Meglévő kód átnézése (BlockMaze, BlockMazeLoader, BlockMazeDisplay)
+- BlockMazeDisplay befejezése, hogy megjelenjen a labirintus
+- Camera, 3rd person character controller, hogy lehessen mászkálni a labirintusban
+- Modell bővítése
+  - Ajtó (nem csak fal, állapot: nyitva/zárva)
+  - Kulcs (felvehető, ajtót nyitja)  
+  - Kezdő pont, cél
+
+### Házi feladat
+
+- Cella+Fal felépítésű labirintus modell
+- Generáló algoritmus adaptálása
+- Ajtók + kulcsok dinamikus elhelyezése 
+- Minimap (falak, tárgyak)
+- Útvonal a célhoz (nyíl / minimap)
+  
+### Linkek
+
+[Daedalus labirintus generáló](http://www.astrolog.org/labyrnth.htm)
