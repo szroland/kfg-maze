a29a1489e5384da65d2b2e846275fb4cc92e00e7
szroland <szroland@freemail.hu>
2016-03-16 13:58:04 +0100

    Objektum interakcio 1.

---
+++ Assets/Scripts/Interaktiv.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Interaktiv : MonoBehaviour {
+
+    new private Camera camera;
+
+    public Text kijelzo;
+
+	void Start () {
+        camera = GetComponentInChildren<Camera>();	
+	}
+	
+	void Update () {
+
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            if (kijelzo)
+            {
+                kijelzo.text = "Fókusz: " + objectHit.name;
+            }
+            // Do something with the object that was hit by the raycast.
+        }
+
+    }
+}
